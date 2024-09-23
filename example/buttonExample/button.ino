6f3baa44a77fa2037f7dbfb9a06c345609802f4e
pooria <31559510+Pooria-nek@users.noreply.github.com>
2024-09-23 10:17:55 +0330

    Update button.ino

--- example/buttonExample/button.ino
+++ example/buttonExample/button.ino
@@ -1,41 +1,32 @@
 #include <deviceSetup.h>
 
-// Declare callback functions for Button 1
-void onButton1ShortPress();
-void onButton1LongPress();
-
-// Declare callback functions for Button 2
-void onButton2ShortPress();
-void onButton2LongPress();
-
-// Create deviceSetup objects for the buttons after function declarations
+// Button objects
 deviceSetup button1(PIN_BUTTON_1, 1000, 2000, onButton1ShortPress, onButton1LongPress);
 deviceSetup button2(PIN_BUTTON_2, 1000, 2000, onButton2ShortPress, onButton2LongPress);
 
 void setup() {
-    // Any necessary setup code
+    // Initialize any additional components here
 }
 
 void loop() {
-    // Continuously check the state of each button
     button1.readButton();
     button2.readButton();
 }
 
-// Define the callback functions for Button 1
+// Callback functions for Button 1
 void onButton1ShortPress() {
-    // Action for Button 1 short press
+    Serial.println("Button 1 short press detected");
 }
 
 void onButton1LongPress() {
-    // Action for Button 1 long press
+    Serial.println("Button 1 long press detected");
 }
 
-// Define the callback functions for Button 2
+// Callback functions for Button 2
 void onButton2ShortPress() {
-    // Action for Button 2 short press
+    Serial.println("Button 2 short press detected");
 }
 
 void onButton2LongPress() {
-    // Action for Button 2 long press
+    Serial.println("Button 2 long press detected");
 }
